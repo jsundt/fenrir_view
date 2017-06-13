73a3e2ab29968503af0db00c7d1be068bc28bd67
Jørgen Sundt <jorgen@theeleven.co.uk>
2017-06-13 15:50:19 +0100

    Added javascript erbs and fixed sass error if you're missing a file

--- app/assets/javascripts/fenrir_view/styleguide.js
+++ app/assets/javascripts/fenrir_view/styleguide.js
@@ -1,5 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-//= require fenrir_view
 //= require fenrir_view/prism
 //= require fenrir_view/sidebar
+
+//= require elements
+//= require components
+//= require modules
+
