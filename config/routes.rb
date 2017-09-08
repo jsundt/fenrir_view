f309f4e3be865793d8c071fae20e6ebc60bb7488
Jørgen Sundt <jorgen@theeleven.co.uk>
2017-09-08 11:19:17 +0100

    Index page for components

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 FenrirView::Engine.routes.draw do
   root to: "docs#index"
 
-  resources :styleguide, only: [:index, :show], path: FenrirView.configuration.styleguide_path
-
+  # resources :styleguide, only: [:index, :show], path: FenrirView.configuration.styleguide_path
+  get "styleguide/", to: "styleguide#index", as: "component_index"
   get "styleguide/:variant/:id", to: 'styleguide#show', as: 'components'
 
   get 'docs/:section(/:page)', to: 'docs#show', as: 'fenrir_docs'
