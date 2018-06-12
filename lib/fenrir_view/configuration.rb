0094df6e17dc140f308eef0fd08699be5ebf51c0
Jørgen Sundt <jorgen@theeleven.co.uk>
2018-06-12 17:29:30 +0100

    Validation of properties for each design system component

--- lib/fenrir_view/configuration.rb
+++ lib/fenrir_view/configuration.rb
@@ -2,11 +2,11 @@ module FenrirView
   class Configuration
     attr_accessor :styleguide_path
     attr_accessor :system_variants
+    attr_accessor :property_validation
     attr_reader :system_path
     attr_reader :docs_path
 
-    def initialize
-    end
+    def initialize; end
 
     def system_path=(path)
       @system_path = Pathname.new(path)
--- lib/fenrir_view/presenter.rb
+++ lib/fenrir_view/presenter.rb
@@ -9,6 +9,7 @@ module FenrirView
       @variant = variant
       @slug = slug
       @properties = default_properties.deep_merge(properties)
+      validate_properties if FenrirView.configuration.property_validation
     end
 
     def render(context, &block)
@@ -23,6 +24,28 @@ module FenrirView
     end
 
     private
+
+    def validate_properties
+      validations_for_component = self.class._properties
+      return unless validations_for_component.any?
+
+      if !properties.is_a?(Hash) && properties != false
+        raise "An instance of #{slug} has properties as #{properties.class.name}. Should be Hash or false."
+      elsif validations_for_component.keys != properties.keys
+        unknown_keys = properties.keys.reject { |key| validations_for_component.include?(key) }
+        raise "#{self.class.name} has unkown keys: #{unknown_keys.join(', ')}"
+      end
+
+      properties.each do |k, v|
+        property_validations = validations_for_component[k]
+
+        if v.blank?
+          raise "An instance of #{slug} is missing the required property: #{k}" if property_validations[:required].presence
+        elsif property_validations && property_validations[:valid_types].present?
+          raise "An instance of #{slug} has the wrong type: '#{v.class}' for property: '#{k}'. Should be one of: #{property_validations[:valid_types]}" if property_validations[:valid_types].include?(v.class)
+        end
+      end
+    end
 
     def default_properties
       self.class._properties.inject({}) do |sum, (k, v)|
