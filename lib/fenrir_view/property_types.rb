1e86da1e98ae1bb7db9518e0ab672dfd3c5f8b43
Jørgen Sundt <jorgen@theeleven.co.uk>
2018-07-04 15:54:46 +0100

    Validate keys in hashes in arrays in properties etc

--- lib/fenrir_view/property_types.rb
+++ lib/fenrir_view/property_types.rb
@@ -8,8 +8,8 @@ module FenrirView
       @instance_properties = instance_properties
     end
 
-    ALL_TYPES = [:default, :required, :one_of_type, :one_of, :note]
-    VALIDATION_TYPES = [:required, :one_of_type, :one_of]
+    VALID_KEYS = [:required, :one_of_type, :one_of, :array_of, :hash_of, :deprecated, :default, :note]
+    VALIDATION_KEYS = [:required, :one_of_type, :one_of, :array_of, :hash_of, :deprecated]
 
     def validate_properties
       return if @instance_properties == false # Rendering of this instance has been blocked by a guard clause
@@ -17,21 +17,37 @@ module FenrirView
       raise unknown_keys if instance_has_unknown_keys?
 
       @instance_properties.each do |property, value|
-        properties = @component_properties[property]
-        property_validations = properties.slice(*VALIDATION_TYPES)
+        validate_property(property, value, @component_properties[property])
+      end
+    end
 
-        property_validations.each do |validation_type, validation_rule|
-          case validation_type
-          when :required
-            raise required_property_missing(property, value) if value.blank?
-          when :one_of_type
-            raise invalid_property_type(property, value, validation_rule) if value.present? && !validation_rule.include?(value.class)
-          when :one_of
-            raise invalid_property_value(property, value, validation_rule) if value.present? && !validation_rule.include?(value)
+    def validate_property(property, value, property_validations)
+      raise deprecated_property(property) if !value.nil? && !!property_validations[:deprecated]
+      raise unknown_validation(property_validations.except(*VALID_KEYS)) if property_validations.except(*VALID_KEYS).any?
+
+      property_validations.slice(*VALIDATION_KEYS).each do |validation_type, validation_rule|
+        case validation_type
+        when :required
+          raise required_property_missing(property, value) if value.blank?
+        when :one_of_type
+          raise invalid_property_type(property, value, validation_rule) if value.present? && !validation_rule.include?(value.class)
+        when :one_of
+          raise invalid_property_value(property, value, validation_rule) if value.present? && !validation_rule.include?(value)
+        when :array_of
+          raise invalid_property_type(property, value, validation_rule) if value.present? && !value.is_a?(Array)
+
+          value.each_with_index do |v, i|
+            validate_property("#{property}[#{i}]", v, property_validations[:array_of])
+          end
+        when :hash_of
+          raise invalid_property_type(property, value, validation_rule) if value.present? && !value.is_a?(Hash)
+
+          # Loop over the validations for the keys in this hash
+          # and check that the 'value' conforms to those rules
+          property_validations[:hash_of].each do |k, key_validations|
+            validate_property("#{property}[#{k}]", value[k], key_validations)
           end
         end
-
-        raise unknown_validation(properties.except(*ALL_TYPES)) if properties.except(*ALL_TYPES).any?
       end
     end
 
@@ -40,11 +56,19 @@ module FenrirView
 
       @component_properties.map do |name, data|
         prop_map[name] = {
-          default: data[:default] || 'nil',
-          required: data[:required] ? 'Required' : 'Optional',
-          one_of_type: data[:one_of_type]&.join(', ') || 'Any type',
-          one_of: data[:one_of]&.join(', ') || 'Any value',
-          note: data[:note] || '',
+          info: [
+            (data[:default] ? ": #{default_value_with_visible_type(data[:default])}" : ''),
+            ('⚠️ Deprecated' if data[:deprecated]),
+            ('Required' if data[:required]),
+            ("As #{data[:one_of_type].join(' or ')}" if data[:one_of_type]),
+          ].compact.join('. '),
+          note: data[:note],
+          validations: {
+            # one_of_type: data[:one_of_type]&.join(', '),
+            one_of: data[:one_of],
+            array_of: data[:array_of],
+            hash_of: data[:hash_of],
+          }.compact,
         }
       end
 
@@ -52,6 +76,16 @@ module FenrirView
     end
 
     private
+
+    def default_value_with_visible_type(property)
+      if property.is_a?(String)
+        "'#{property.blank? ? ' ' : property}'"
+      elsif property.is_a?(Symbol)
+        ":#{property}"
+      else
+        property
+      end
+    end
 
     def invalid_component_properties
       ArgumentError.new("An instance of #{@component_class.name} has properties as #{@instance_properties.class.name}. Should be Hash or false.")
@@ -64,6 +98,10 @@ module FenrirView
     def unknown_keys
       unknown_keys = @instance_properties.keys.reject { |key| @component_properties.include?(key) }
       ArgumentError.new("#{@component_class.name} has unkown keys: #{unknown_keys.join(', ')}. Should be one of: #{@component_properties.keys.join(', ')}")
+    end
+
+    def deprecated_property(property)
+      ArgumentError.new("An instance of #{@component_class.name} is using the deprecated property #{property}")
     end
 
     def required_property_missing(property, value)
@@ -79,7 +117,7 @@ module FenrirView
     end
 
     def unknown_validation(unkown_validations)
-      ArgumentError.new("#{@component_class.name} has unkown property validations: #{unkown_validations.keys.join(', ')}. Should be one of: #{ALL_TYPES.join(', ')}")
+      ArgumentError.new("#{@component_class.name} has unkown property validations: #{unkown_validations.keys.join(', ')}. Should be one of: #{VALID_KEYS.join(', ')}")
     end
   end
 end
