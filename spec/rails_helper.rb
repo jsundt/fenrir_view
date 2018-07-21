93b18901a6e367711948752ad29b89649114a7cf
Jørgen Sundt <jorgen@theeleven.co.uk>
2018-07-21 21:26:59 +0100

    system test generator

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../test/dummy/config/environment', __FILE__)
+require "generators/fenrir_view/new_pattern_generator"
 
 require 'selenium-webdriver'
 require 'rspec/rails'
