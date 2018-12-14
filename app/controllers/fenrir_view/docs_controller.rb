d370414ff00d5774218f21dfb75e769fc33d85d6
Jørgen Sundt <jorgen@theeleven.co.uk>
2018-12-14 14:56:04 +0000

    Validate documentation routes with your specified pages

--- app/controllers/fenrir_view/docs_controller.rb
+++ app/controllers/fenrir_view/docs_controller.rb
@@ -1,41 +1,31 @@
+# frozen_string_literal: true
+
 module FenrirView
   class DocsController < FenrirController
-
-    before_action :doc_pages
-    before_action :fallback_section
-
     def index
-      redirect_to fenrir_docs_path(section: @section, page: @doc)
+      redirect_to fenrir_docs_path(redirect_options)
     end
 
+    # The parameters used to render the show action is validated
+    # against your own list of specified documentation pages in docs/index.yml
     def show
       @partial = "/#{ @section.to_s }/#{ @doc.to_s }"
     end
 
     private
 
-    def fallback_section
-      @section = doc_sections.include?(params[:section]&.to_sym) ? params[:section] : doc_sections.first
-      @doc = params[:page] ? params[:page] : @docs[@docs.keys.first][@section].first[0]
+    def redirect_options
+      section = valid_pages.keys.first.to_s
+      page = valid_pages[section].first.to_s
+
+      {
+        section: section,
+        page: page
+      }
     end
 
-    def doc_sections
-      @sections = []
-
-      @docs.keys.each do |key|
-        @sections += @docs[key].keys
-      end
-
-      @sections
-    end
-
-    def doc_pages
-      yml_docs = YAML.load_file(docs_index_file)
-      @docs = JSON.parse(JSON[yml_docs], symbolize_names: true)
-    end
-
-    def docs_index_file
-      FenrirView.configuration.docs_path.join("index.yml")
+    def valid_pages
+      @valid_pages ||= FenrirView::Documentation.new.valid_pages
     end
   end
 end
