248953caff7a3ca95f82bead99d7e29236422274
Jørgen Sundt <jorgen@theeleven.co.uk>
2018-12-23 07:07:48 +0000

    Fix application frame test

--- spec/fenrir_view/component_helper_spec.rb
+++ spec/fenrir_view/component_helper_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe FenrirView::ComponentHelper, type: :helper do
   let(:initilized_configuration) { FenrirView.configuration }
+  let(:styleguide_facade) { FenrirView::StyleguideFacade.new }
 
   describe '#ui_component' do
     it 'renders components from your app' do
@@ -11,13 +12,13 @@ RSpec.describe FenrirView::ComponentHelper, type: :helper do
     end
 
     it 'does not render components from gem' do
-      expect { helper.ui_component('application_frame', { title: 'test' }) }.to raise_error('Could not find component: components: application_frame')
+      expect { helper.ui_component('application_frame', { page: styleguide_facade }) }.to raise_error('Could not find component: components: application_frame')
     end
   end
 
   describe '#system_component' do
     it 'renders components from gem' do
-      expect { helper.system_component('application_frame', { title: 'test' }) }.to_not raise_error
+      expect { helper.system_component('application_frame', { page: styleguide_facade }) }.to_not raise_error
     end
 
     it 'does not render components from your app' do
