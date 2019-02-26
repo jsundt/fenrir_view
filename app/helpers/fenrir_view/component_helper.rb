36beb30d6f938747ff6046d55bb3bc423807d984
Jørgen Sundt <jorgen@theeleven.co.uk>
2019-02-26 16:43:30 +0000

    Add ability to pass multiple blocks to components

--- app/helpers/fenrir_view/component_helper.rb
+++ app/helpers/fenrir_view/component_helper.rb
@@ -1,23 +1,38 @@
+# frozen_string_literal: true
+
 module FenrirView
   module ComponentHelper
-    def render_ui(variant, slug, properties = {}, &block)
+    def ui_component(slug, properties = {}, &_block)
       return nil if properties == false
 
-      component = FenrirView::Presenter.component_for(variant, slug, properties, validate: true)
+      component = FenrirView::Presenter.component_for('components', slug, properties, validate: true)
+
+      if block_given?
+        if component.respond_to?(:layout)
+          component.build_layout_from_context(self)
+
+          # This capture is not passed to the generic content_block,
+          # as we are making the different parts of the block available
+          # to specific parts of the component partial, instead of the
+          # regular yield block.
+          capture { yield component.layout }
+        else
+          content_block = capture { yield }
+        end
+      end
+
       component.render(controller.view_context) do
-        capture(&block) if block_given?
+        content_block
       end
     end
 
-    def ui_component(slug, properties = {}, &block)
-      render_ui('components', slug, properties) do
-        capture(&block) if block_given?
-      end
-    end
+    def system_component(slug, properties = {}, &_block)
+      return nil if properties == false
 
-    def system_component(slug, properties = {}, &block)
-      render_ui('system', "fenrir_view_#{slug}", properties) do
-        capture(&block) if block_given?
+      component = FenrirView::Presenter.component_for('system', "fenrir_view_#{slug}", properties, validate: true)
+
+      component.render(controller.view_context) do
+        capture { yield } if block_given?
       end
     end
   end
